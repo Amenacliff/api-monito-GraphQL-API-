59983993d764ec2bfa3d0406d24ab063ebc5a151
Amenacliff <amenaclifford@gmail.com>
2022-07-11 21:14:00 +0100

    Initialzied TypeORM & Configs

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+namespace NodeJS {
+    interface ProcessEnv {
+        POSTGRESQL_PORT : string,
+        POSTGRESQL_DBNAME : string,
+        POSTGRESQL_USER_NAME : string
+    }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const TypeOrmConfig = TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    host: 'localhost',
+    port: Number(config.get('POSTGRESQL_PORT')),
+    username: config.get('POSTGRESQL_USER_NAME'),
+    password: '',
+    database: config.get('POSTGRESQL_DBNAME'),
+    entities: [],
+    synchronize: true,
+  }),
+});
+
+const configModuleConfig = ConfigModule.forRoot({
+  isGlobal: true,
+});
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, configModuleConfig, TypeOrmConfig],
   controllers: [AppController],
   providers: [AppService],
 })
